d252ce0dd788e2557a41eac5be09c72c02d89e69
yh-orange <1184126912@qq.com>
2022-01-24 11:34:16 +0800

    Vuepress dome init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,42 +1,61 @@
 module.exports = {
-    title: 'vue express dome',
-    description: 'dome',
+    title: 'yh-orange 学习笔记',
+    description: 'Personal Website',
+    head: [ // 注入到当前页面的 HTML <head> 中的标签
+        ['link', {rel: 'icon', href: '/images/icon.png'}],
+        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+
+    ],
+    serviceWorker: true, // 是否开启 PWA
+    markdown: {
+        lineNumbers: true // 代码块是否显示行号
+    },
     theme: 'reco',
-    locales: {
-        '/': {
-            lang: 'zh-CN'
+    port: 8080, //  指定 dev server 的端口。
+    themeConfig: {
+        logo: '/images/hero.jpg',
+        author: 'yh-orange',
+        valineConfig: {
+            appId: 'CB0HAiNw5GUiv2sqoheMPDLy-gzGzoHsz',// your appId
+            appKey: 'yI54eHlvB55dsvzwFgVMYdeq', // your appKey
+        },
+        // 博客配置
+        // blogConfig: {
+        //     category: {
+        //         location: 2,     // 在导航栏菜单中所占的位置，默认2
+        //         text: 'Category' // 默认文案 “分类”
+        //     },
+        //     tag: {
+        //         location: 3,     // 在导航栏菜单中所占的位置，默认3
+        //         text: 'Tag'      // 默认文案 “标签”
+        //     },
+        //     socialLinks: [     // 信息栏展示社交信息
+        //         { icon: 'reco-github', link: 'https://github.com/recoluan' },
+        //         { icon: 'reco-npm', link: 'https://www.npmjs.com/~reco_luan' }
+        //     ]
+        // },
+        nav: [ // 导航栏配置
+            {text: '前端基础资料', link: '/base-js/'},
+            {text: 'Markdown学习笔记', link: '/md-study-note/'},
+            {text: '自己的网站', link: 'http://120.77.15.133'},
+            {text: 'TimeLine', link: '/timeline/', icon: 'reco-date'}
+        ],
+        // sidebar: [
+        //     {
+        //         title: "Markdown学习笔记",
+        //         path: '/md-study-note/',
+        //         collapsable: false, // 不折叠
+        //     }
+        // ],
+        // subSidebar: 'auto',
+        lastUpdated: 'Last Updated', // string | boolean
+        sidebar: 'auto', // 侧边栏配置
+        sidebarDepth: 3
+    },
+    configureWebpack: {
+        resolve: {
+            alias: {}
         }
     },
-    themeConfig: {
-        subSidebar: 'auto',
-        nav: [
-            { text: '首页', link: '/' },
-            {
-                text: '告鸟 dome',
-                items: [
-                    { text: 'Github', link: 'https://github.com/yh-orange' },
-                    { text: '掘金', link: 'https://juejin.cn' }
-                ]
-            }
-        ],
-        sidebar: [
-            {
-                title: '欢迎学习',
-                path: '/',
-                collapsable: true, // 不折叠
-                children: [
-                    { title: "学前必读", path: "/" }
-                ]
-            },
-            {
-                title: "基础学习",
-                path: '/handbook/ConditionalTypes',
-                collapsable: true, // 不折叠
-                children: [
-                    { title: "条件类型", path: "/handbook/ConditionalTypes" },
-                    { title: "泛型", path: "/handbook/Generics" }
-                ],
-            }
-        ]
-    }
-}
+    base: '/' // 部署到github相关的配置
+};
--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,15 @@
-'# Hello VuePress' 
+---
+home: true
+heroImage: /images/hero.jpg
+heroText: 愿你每一份努力都终于所成
+tagline: 
+features:
+- title: 
+  details: 
+- title: 鸡汤
+  details: 以后的你会感谢现在努力的你。
+- title: 
+  details: 
+footer: MIT Licensed | Copyright © 2018-present Evan You
+---
 
----
-title: 条件类型
-author: 冴羽
-date: '2021-12-12'
----
