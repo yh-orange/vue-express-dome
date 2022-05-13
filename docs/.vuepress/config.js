d03113f92547f4e0512b8f82a0c8175b45c9f8f2
yh-orange <1184126912@qq.com>
2022-05-13 19:14:55 +0800

    nginx 知识补充

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -63,7 +63,7 @@ module.exports = {
                         { title: 'javaScript基础知识', path: '/base-js/javaScript基础知识' },
                         { title: 'ES6-ES12特性总结', path: '/base-js/ES6-ES12特性总结' },
                         { title: 'HTTP 相关知识', path: '/base-js/HTTP相关知识' },
-                        { title: '浏览器对象', path: '/base-js/浏览器对象' }
+                        { title: 'Nginx相关知识', path: '/base-js/Nginx' }
                         // { title: '第三节', path: '/study/study03' }
                         // '/base-js/javaScript基础知识',
                         // '/base-js/javaScript基础知识',
